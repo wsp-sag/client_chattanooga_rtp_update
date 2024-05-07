e71d409c6260b6a58f1ede007eead81751d46e57
Alice Huang <55039851+xtHuang0927@users.noreply.github.com>
2024-05-06 23:32:02 -0700

    Update map automation section in run_analysis.py

--- summary_tool/run_analysis.py
+++ summary_tool/run_analysis.py
@@ -5,12 +5,14 @@ from pathlib import Path
 import yaml
 import pandas
 import geopandas
+import subprocess
 
 from typing import Optional, Literal
 
 import scripts._Bridge_Report.bridge_report as bridge_report
 import scripts._Project_Evaluation.project_evaluation as project_evaluation
 import scripts._Scenario_Impact.scenario_impact as scenario_impact
+import scripts._Map_Automation.build_map_files as build_map_files
 
 
 cd = Path(os.getcwd())
@@ -18,6 +20,7 @@ cd = Path(os.getcwd())
 MODEL_OUTPUT_PATH = cd / "model_output"
 SUMMARY_OUTPUT_PATH = cd / "summary_output"
 LINK_QUERY_LOOKUP = cd / "lookups"
+MAP_DATA_PATH = cd / "scripts" / "_Map_Automation" / "Data"
 
 
 def get_config():
@@ -69,6 +72,29 @@ if __name__ == "__main__":
         )
         print("finish project evaluation report")
 
+    
+    if config["map_automation"]["OUTPUT_POSTPROCESS"]:
+        print("postprocessing model outputs")
+        SHP_SAVE_PATH = MAP_DATA_PATH / "Model_Output"
+        build_map_files.model_output_postprocess(
+            MODEL_OUTPUT_PATH,
+            MAP_DATA_PATH,
+            SHP_SAVE_PATH,
+            config["map_automation"]["scenario"],
+            LINK_QUERY_LOOKUP
+        )
+        print("finish model outputs postprocess")
+
+        if config["map_automation"]["CREATE_MAP_OUTPUT"]:
+            print("generating pdf maps")
+            generate_map_script =  cd / "scripts" / "_Map_Automation" / "Maps_Automate.py"
+            qgis_python_path = config['map_automation']['qgis_python_path']
+            python_executable_path = config['map_automation']['python_executable_path']
+            
+            os.environ["PYTHONPATH"] = qgis_python_path
+            subprocess.run([python_executable_path, generate_map_script], shell=True)
+            print("finish pdf maps")
+   
+
     print("done")
 
-# %%
